748ef9b0b2388c68cce4983b5cb4cb202100c8ea
aravk <aravk@altair.com>
2024-02-26 11:04:41 +0530

    Optimizing for ssh

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -8,4 +8,6 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.animate", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  -- { "echasnovski/mini.indentscope", enabled = false },
 }
