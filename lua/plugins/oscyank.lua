c74755adf4513304e4d1e5dcaaea8d58e0a313d7
aravk <aravk@altair.com>
2024-04-15 14:38:23 +0530

    OSC related

--- lua/plugins/oscyank.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ojroques/vim-oscyank",
-    branch = "main",
-    keys = {
-        { "<leader>y", '<Plug>OSCYankOperator', mode="n", desc="Yank OSC" },
-        { "<leader>y", '<Plug>OSCYankVisual', mode="v", desc="Yank OSC Visual" },
-    },
-}
